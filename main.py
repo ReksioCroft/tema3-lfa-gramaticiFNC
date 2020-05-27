ed85fdc72b27521fb59723c8f838826e7db47da4
Octavian-Florin Staicu 131 <octavian-florin.staicu@my.fmi.unibuc.ro>
2020-05-27 22:08:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+def eliminareLProductii( gramatica ):
+    ok = 0
+    while ok == 0:
+        ok = 1
+        simbol = None
+        for i in gramatica:
+            if '$' in gramatica[ i ]:
+                simbol = i
+                ok = 0
+                break
+        if simbol is not None:
+            if len( gramatica[ simbol ] ) == 1:
+                gramatica.pop( simbol, None )
+                for i in gramatica:
+                    for j in range( len( gramatica[ i ] ) ):
+                        if gramatica[ i ][ j ] == simbol:
+                            gramatica[ i ][ j ] = '$'
+                        else:
+                            gramatica[ i ][ j ] = gramatica[ i ][ j ].replace( simbol, "" )
+                            if len( gramatica[ i ][ j ] ) == 0:
+                                gramatica[ i ].pop( j )
+            else:
+                gramatica[ simbol ].pop( gramatica[ simbol ].index( '$' ) )
+                for i in gramatica:
+                    if i != simbol:
+                        for j in range( len( gramatica[ i ] ) ):
+                            if simbol in gramatica[ i ][ j ]:
+                                s = gramatica[ i ][ j ].replace( simbol, "" )
+                                if len( s ) > 0:
+                                    gramatica[ i ].append( gramatica[ i ][ j ].replace( simbol, "" ) )
+    return gramatica
+
+
+fin = open( "date.in" )
+gramatica = dict()
+for i in fin:
+    l = i.split( "->" )
+    gramatica[ l[ 0 ].strip() ] = [ x.strip( "|" ) for x in l[ 1 ].replace( "|", " " ).split() ]
+print( gramatica )
+
+gramatica = eliminareLProductii( gramatica )
+print( gramatica )
